bc0e45a84b2f80bcd555d9122273617f454d7759
Jobayer Hossan <jobayerhossan34@gmail.com>
2026-04-17 11:13:40 +0600

    Intial Release

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,13 @@ import "./globals.css";
 const headingFont = Space_Grotesk({
   variable: "--font-heading",
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 const monoFont = IBM_Plex_Mono({
   variable: "--font-mono",
   subsets: ["latin"],
+  weight: ["400", "500"],
 });
 
 export const metadata: Metadata = {
